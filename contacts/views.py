92d1e1a85bd17cc3738ae7b1ca9be801d1b6b055
dibidibooo <alaska.lubit.tebya@gmail.com>
2022-03-16 15:06:50 +0600

    add new users html

--- contacts/views.py
+++ contacts/views.py
@@ -1,25 +1,29 @@
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 
 class UserGridView(LoginRequiredMixin,View):
     def get(self , request):
-        greeting = {}
-        greeting['heading'] = "User Grid"
-        greeting['pageview'] = "Contacts"
-        return render (request,'contacts/usergrid.html',greeting)
+        context = {
+            'heading':"User Grid",
+            'pageview':"Contacts",
+        }
+        return render (request,'contacts/usergrid.html',context)
 
 class UserListView(LoginRequiredMixin,View):
     def get(self , request):
-        greeting = {}
-        greeting['heading'] = "User List"
-        greeting['pageview'] = "Contacts"
-        return render (request,'contacts/userlist.html',greeting)
+        context = {
+            'heading':"User List",
+            'pageview':"Contacts",
+        }
+        return render (request,'contacts/userlist.html',context)
 
 class ProfileView(LoginRequiredMixin,View):
     def get(self , request):
-        greeting = {}
-        greeting['heading'] = "Profile"
-        greeting['pageview'] = "Contacts"
-        return render (request,'contacts/profile.html',greeting)
+        context = {
+            'heading':"Profile",
+            'pageview':"Contacts",
+        }
+        return render (request,'contacts/profile.html',context)
