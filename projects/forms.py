535cbb9299d9e63ad6ce09ce593366d9f9926a45
dibidibooo <alaska.lubit.tebya@gmail.com>
2022-03-17 17:42:10 +0600

    dot

---
+++ projects/forms.py
@@ -0,0 +1,65 @@
+from django import forms
+
+
+class MultipleForm(forms.Form):
+    action = forms.CharField(max_length=60, widget=forms.HiddenInput())
+
+
+class Site1Form(MultipleForm):
+    oil_prod = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}), label="Нефтепродукт")
+    suspended_subst = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}), label="Взвешенные вещества")
+    ph = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}), label="pH")
+    oxygen_chem = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}), label="Химическая потребность в кислороде (ХПК)")
+    active_subst = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}), label="Поверхностно-активные вещества  (АПАВ)")
+    ammonium = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}), label="Аммонийный азот")
+
+
+class Site2Form(MultipleForm):
+    oil_prod = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}),
+                               label="Нефтепродукт")
+    suspended_subst = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}),
+                                      label="Взвешенные вещества")
+    ph = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}), label="pH")
+    oxygen_chem = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}),
+                                  label="Химическая потребность в кислороде (ХПК)")
+    active_subst = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}),
+                                   label="Поверхностно-активные вещества  (АПАВ)")
+    ammonium = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}),
+                               label="Аммонийный азот")
+    oxygen_bio = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}),
+                               label="Биологическая потребность в кислороде (БПК5)")
+    phenols = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}),
+                               label="Фенолы")
+    chlorides = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}),
+                               label="Хлориды")
+    sulfates = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}),
+                               label="Сульфаты")
+    iron = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}),
+                               label="Железо")
+    nitrate = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}),
+                               label="Азот нитратов")
+    nitrite = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}),
+                               label="Азот нитритов")
+
+
+# class Site3Form(MultipleForm):
+#     alkalinity = forms.IntegerField(widget=forms.IntegerField)
+#     hardness = forms.IntegerField(widget=forms.IntegerField)
+#     oxidability = forms.IntegerField(widget=forms.IntegerField)
+#     salt = forms.IntegerField(widget=forms.IntegerField)
+#     chlorine = forms.IntegerField(widget=forms.IntegerField)
+#
+#
+# class Site4Form(MultipleForm):
+#     oil_prod = forms.IntegerField(widget=forms.IntegerField)
+#     suspended_solids = forms.IntegerField(widget=forms.IntegerField)
+#     ph = forms.IntegerField(widget=forms.IntegerField)
+#     phosphorus = forms.IntegerField(widget=forms.IntegerField)
+#     alkalinity = forms.IntegerField(widget=forms.IntegerField)
+#     hardness = forms.IntegerField(widget=forms.IntegerField)
+#     salt = forms.IntegerField(widget=forms.IntegerField)
+#     chlorides = forms.IntegerField(widget=forms.IntegerField)
+#     sulfates = forms.IntegerField(widget=forms.IntegerField)
+#     iron = forms.IntegerField(widget=forms.IntegerField)
+#     hardness_calcium = forms.IntegerField(widget=forms.IntegerField)
+#     hardness_magnesium = forms.IntegerField(widget=forms.IntegerField)
--- projects/views.py
+++ projects/views.py
@@ -1,22 +1,103 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-# from projects.models import (
-#     ComponentsSite1,
-#     ComponentsSite2,
-#     ComponentsSite3,
-#     ComponentsSite4
-# )
+from .forms import Site1Form, Site2Form
+from .models import (
+    ComponentsSite1, ComponentsSite2
+)
+from .multiforms import MultiFormsView
 
 
-class ProjectsGridView(LoginRequiredMixin, View):
-    def get(self, request):
-        context = {
-            'heading': "Загрузка анализов",
-            'pageview': "Projects"
-        }
-        return render(request, 'projects/projectsgrid.html', context)
+class MultipleFormsDemoView(MultiFormsView):
+    template_name = "projects/projectsgrid_test.html"
+    form_classes = {'site1': Site1Form,
+                    'site2': Site2Form,
+                    }
+
+    success_urls = {
+        'site1': reverse_lazy('projects-createview'),
+        'site2': reverse_lazy('projects-createview'),
+    }
+
+    def site1_form_valid(self, form):
+        print(self.request.POST.get('water_type'))
+        oil_prod = form.cleaned_data.get('oil_prod')
+        suspended_subst = form.cleaned_data.get('suspended_subst')
+        ph = form.cleaned_data.get('ph')
+        oxygen_chem = form.cleaned_data.get('oxygen_chem')
+        active_subst = form.cleaned_data.get('active_subst')
+        ammonium = form.cleaned_data.get('ammonium')
+        form_name = form.cleaned_data.get('action')
+        ComponentsSite1.objects.create(
+            oil_prod=oil_prod,
+            suspended_subst=suspended_subst,
+            ph=ph,
+            oxygen_chem=oxygen_chem,
+            active_subst=active_subst,
+            ammonium=ammonium,
+            sampling_site_id=self.request.POST.get('plant_unit'),
+            water_type_id=self.request.POST.get('water_type')
+        )
+        return HttpResponseRedirect(self.get_success_url(form_name))
+
+    def site2_form_valid(self, form):
+        oil_prod = form.cleaned_data.get('oil_prod')
+        suspended_subst = form.cleaned_data.get('suspended_subst')
+        ph = form.cleaned_data.get('ph')
+        oxygen_chem = form.cleaned_data.get('oxygen_chem')
+        active_subst = form.cleaned_data.get('active_subst')
+        ammonium = form.cleaned_data.get('ammonium')
+        oxygen_bio = form.cleaned_data.get('oxygen_bio')
+        phenols = form.cleaned_data.get('phenols')
+        chlorides = form.cleaned_data.get('chlorides')
+        sulfates = form.cleaned_data.get('sulfates')
+        iron = form.cleaned_data.get('iron')
+        nitrate = form.cleaned_data.get('nitrate')
+        nitrite = form.cleaned_data.get('nitrite')
+        form_name = form.cleaned_data.get('action')
+        ComponentsSite2.objects.create(
+            oil_prod=oil_prod,
+            suspended_subst=suspended_subst,
+            ph=ph,
+            oxygen_chem=oxygen_chem,
+            active_subst=active_subst,
+            ammonium=ammonium,
+            oxygen_bio=oxygen_bio,
+            phenols=phenols,
+            chlorides=chlorides,
+            sulfates=sulfates,
+            iron=iron,
+            nitrate=nitrate,
+            nitrite=nitrite,
+            sampling_site_id=1,
+            water_type_id=1
+        )
+        return HttpResponseRedirect(self.get_success_url(form_name))
+
+
+# class AnalysisCreateView(LoginRequiredMixin, CreateView):
+#     template_name = 'projects/projectsgrid_test.html'
+#     context_object_name = 'results'
+#     success_url = reverse_lazy('projects-createview')
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['heading'] = 'Загрузка анализов'
+#         context['pageview'] = 'Projects'
+#         context['components'] = Component.objects.all()
+#         return context
+#
+#     def form_valid(self, form):
+#         result = form.save()
+#         components = Component.objects.all()
+#         for component in components:
+#             if result.component.pk == component.pk:
+#                 pass
+#         # sample = Sample(result_id=result.pk, sampling_site_id=self.request.POST.get('plant_unit'))
+#         return super().form_valid(form)
 
 
 class ProjectsListView(LoginRequiredMixin, View):
@@ -37,21 +118,10 @@ class ProjectOverviewView(LoginRequiredMixin, View):
         return render(request,'projects/projectsoverview.html', context)
 
 
-class CreateViewView(LoginRequiredMixin, View):
+class ResultsView(LoginRequiredMixin, View):
     def get(self, request):
         context = {
             'heading': "Результаты",
             'pageview': "Projects"
         }
-        return render(request,'projects/createnew.html', context)
-
-    def post(self, request, *args, **kwargs):
-        greeting = {}
-        greeting['heading'] = "Create New Project"
-        greeting['pageview'] = "Projects"
-        # site1 = ComponentsSite1
-        # site1.sampling_site.id
-        # site2 = ComponentsSite2
-        # site3 = ComponentsSite3
-        # site4 = ComponentsSite4
-        return render(request, 'projects/projectsgrid_test.html', greeting)
+        return render(request, 'projects/createnew.html', context)
