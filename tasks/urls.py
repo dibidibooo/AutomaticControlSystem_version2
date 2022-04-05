3874f30763e70ec84c299168f8ed952d17c16f1b
Kairat T <kairat@thebeast.local>
2022-04-05 10:56:09 +0600

    Реализовал редактирование задачи. Создал вьюху, форму, путь. Внес соответствующие изменения в шаблон.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+
+from projects.views.tasks import TaskUpdateView
 from tasks import views
 
 urlpatterns = [
-    path('tasklist',views.TaskListView.as_view(),name='tasks-tasklist'),
-    path('kanbanboard',views.KanbanBoardView.as_view(),name='tasks-kanbanboard'),
-    path('createtask',views.CreateTaskView.as_view(),name='tasks-createtask'),
+    path('tasklist', views.TaskListView.as_view(), name='tasks-tasklist'),
+    path('kanbanboard', views.KanbanBoardView.as_view(), name='tasks-kanbanboard'),
+    path('createtask', views.CreateTaskView.as_view(), name='tasks-createtask'),
+    path('task_update/<int:pk>', TaskUpdateView.as_view(), name='tasks-updatetask'),
 ]
