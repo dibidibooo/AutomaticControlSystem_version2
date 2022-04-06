503ec809c5146a01348d7b8ad41bf0a013b81ffd
dibidibooo <alaska.lubit.tebya@gmail.com>
2022-04-06 17:10:44 +0600

    06.04.2022

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('tasklist', views.TaskListView.as_view(), name='tasks-tasklist'),
     path('kanbanboard', views.KanbanBoardView.as_view(), name='tasks-kanbanboard'),
     path('createtask', views.CreateTaskView.as_view(), name='tasks-createtask'),
-    path('kanbanboard/<int:pk>', TaskUpdateView.as_view(), name='tasks-updatetask'),
+    path('task_update/<int:pk>', TaskUpdateView.as_view(), name='tasks-updatetask'),
 ]
