4dc02b0814798e18ba43850e37c24dd8d40c6161
Kairat T <kairat@thebeast.local>
2022-04-20 11:21:51 +0600

    Добавил вывод истории изменения задачи. Внес соответствующие изменения в шаблон, логику. Исправил текст email уведомлений.

--- tasks/signals.py
+++ tasks/signals.py
@@ -16,7 +16,8 @@ def send_task_send_email(sender, instance, created, **kwargs):
         print(f'{instance} has no assignee')
     elif instance.user and instance.user.email:
         subject = f'Изменения в задаче #{instance.id}'
-        body = f'В задаче №{instance.id}:'
+        body = f"""В задаче №{instance.id} - {instance.title} для компонента {instance.comp_title}.
+        (Место отбора проб: {instance.sampling_site}. Установка: {instance.plant_unit}.):"""
         if instance.tracker.has_changed('status_id'):
             body += f'\n- изменен статус c "{Status.objects.get(id=instance.tracker.previous("status_id"))}" на "{task.status}".'
         if instance.tracker.has_changed('user_id'):
