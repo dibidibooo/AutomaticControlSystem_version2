5f3f3c5725f51e7943962add9b0665715967c874
Kairat T <kairat@thebeast.local>
2022-04-28 14:52:06 +0600

    Незначительные изменения. Поменял названия url-путей.

--- projects/urls.py
+++ projects/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from .views.analysis import AnalysisCreateView, AdditionalAnalysisCreateView, ProjectOverviewView, ResultsView
 
 urlpatterns = [
-    path('analyzes', AnalysisCreateView.as_view(), name='projects-projectsgrid'),
-    path('projectslist', AdditionalAnalysisCreateView.as_view(), name='projects-projectslist'),
-    path('table', ProjectOverviewView.as_view(), name='projects-projectoverview'),
-    path('results', ResultsView.as_view(), name='projects-createview'),
-
+    path('analyzes', AnalysisCreateView.as_view(), name='analyzes_create'),
+    path('analyzes/additional', AdditionalAnalysisCreateView.as_view(), name='additional_analyzes_create'),
+    path('table', ProjectOverviewView.as_view(), name='excel_table'),
+    path('results', ResultsView.as_view(), name='analyzes_results'),
 ]
