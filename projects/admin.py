debaa4f38ba0871baa568a6315c2055427d8b429
dibidibooo <alaska.lubit.tebya@gmail.com>
2022-05-11 15:14:20 +0600

    ненужный коммит

--- projects/admin.py
+++ projects/admin.py
@@ -5,6 +5,7 @@ from .models import (
     SamplingSite,
     WaterType,
     Component,
+    ComponentsSite,
     # ComponentsSite1,
     # ComponentsSite2,
     # ComponentsSite3,
@@ -29,6 +30,7 @@ admin.site.register(PlantUnit)
 admin.site.register(SamplingSite)
 admin.site.register(WaterType)
 admin.site.register(Component)
+admin.site.register(ComponentsSite)
 # admin.site.register(ComponentsSite1)
 # admin.site.register(ComponentsSite2)
 # admin.site.register(ComponentsSite3)
